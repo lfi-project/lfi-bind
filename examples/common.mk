caa0675741c530177592fdd34236dfc3f7c15573
Zachary Yedidia <zyedidia@gmail.com>
2025-07-09 13:43:44 -0700

    Use meson for testing

--- examples/common.mk
+++
@@ -1,8 +0,0 @@
-ROOT=$(dir $(realpath $(lastword $(MAKEFILE_LIST))))/..
-
-ARCH ?= $(shell uname -m)
-LFIBIND ?= $(ROOT)/lfi-bind
-LFICC ?= $(ARCH)-lfi-linux-musl-clang
-CC ?= cc
-LIBLFI ?= $(ROOT)/examples/lfi-runtime/install/lib/liblfi.a
-INCLUDELFI ?= $(ROOT)/examples/lfi-runtime/install/include
--- examples/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/lfi-project/lfi-runtime
-cd lfi-runtime
-meson setup --prefix=$PWD/install --libdir=lib build -Dbuildtype=debugoptimized
-cd build
-ninja install
